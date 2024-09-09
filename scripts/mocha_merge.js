90efbd0bb35ef8c3b03b3bf84e8b2006412584d3
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2024-09-09 15:03:50 +0300

    upd

--- scripts/mocha_merge.js
+++ scripts/mocha_merge.js
@@ -8,11 +8,10 @@ const parser = new ArgumentParser({
 parser.add_argument('--build', { help: 'Build number (optional)' })
 parser.add_argument('--branch', { help: 'The git branch that Cypress ran against' })
 parser.add_argument('--browser', { help: 'The browser that Cypress used (ie, Chrome, Firefox)', required: true })
-parser.add_argument('-e', '--environment', { help: 'The app environment Cypress ran against (ie, staging, production)', required: true })
 
 async function mocha_merge() {
-  const { build, branch, browser, environment } = getArgs()
-  let title = `Cypress Automation for ${browser} on ${environment}.`
+  const { build, branch, browser } = getArgs()
+  let title = `Cypress Automation for ${browser}`
   title = branch !== null ? `${title} ${branch}` : title
   title = branch !== null ? `${title}#${build}` : title
   const jsonReport = await merge({ files: ['cypress/mochawesome/report/*.json'] })
